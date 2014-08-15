2ab40ec4676d63c59c5e81223f39129ffe0a58cc
oldwang <b00902126@ntu.edu.tw>
2014-08-15 15:34:31 +0800

    fix class id with alphabet

--- chrome_extension/ns.js
+++ chrome_extension/ns.js
@@ -64,7 +64,7 @@ function on_click_tr(event, tr)
   event.data[0].new_row = new_row;
   new_column = $('<td>').appendTo(new_row).attr('colspan', '17');
                   
-  url = 'http://ntuscoreapi.herokuapp.com/score/' + thisYear  + '/' + thisSe + '/' + event.data[1].replace(/\D/g, '+') + '/' + event.data[2];
+  url = 'http://ntuscoreapi.herokuapp.com/score/' + thisYear  + '/' + thisSe + '/' + event.data[1].replace(/\S/g, '+') + '/' + event.data[2];
   // console.log(url)
 
   $.get(url, 
