ebaa807106f69a83f79fdbd6c27d4398e95c61b9
oldwang <b00902126@ntu.edu.tw>
2014-08-15 10:17:12 +0800

    API Complete

---
+++ README.md
@@ -0,0 +1,6 @@
+	
+Simple API
+
+http://if163.aca.ntu.edu.tw/eportfolio/student/Curve.asp?Year=101&Term=1&CouCode=103+51650&Class=02
+
+http://localhost:9527/score/102/1/103+51650/02
---
+++ app.js
@@ -0,0 +1,85 @@
+var express = require('express');
+var http = require('http');
+var request = require('request');
+var cheerio = require('cheerio');
+var app = express();
+
+
+app.set('port', process.env.PORT || 9527);
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.get('/', function(req, res){
+    return res.send('Error 404: No course found');
+});
+
+app.get('/score/:year/:term/:ccode/:cnumber', function(req, res){
+
+	var year = parseInt(req.params.year),
+		term = req.params.term,
+		CouCode = req.params.ccode,
+		classNumber = req.params.cnumber
+
+	var scoreArr1 = [],
+		scoreArr2 = [],
+		scoreArr3 = []
+	var webPrefix = "http://if163.aca.ntu.edu.tw/eportfolio/student/Curve.asp?Year=" 
+	var webSufix1 = (year - 0 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class=" + classNumber
+	var webSufix2 = (year - 1 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class=" + classNumber
+	var webSufix3 = (year - 2 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class=" + classNumber
+
+	request(webPrefix + webSufix1, function (err, rsp, html) {
+		if (!err && rsp.statusCode == 200) {
+		    var $ = cheerio.load(html);
+		    $('font').each(function(i, element){
+		    	var a = $(this).text()
+		    	var num = a.match(/\d+/)
+		    	console.log(num[0]);
+		    	scoreArr1.push(num[0])
+		    });
+		} // Success fst year
+		else {
+			console.log("empty")
+		}
+		request(webPrefix + webSufix2, function (err, rsp, html) {
+			if (!err && rsp.statusCode == 200) {
+			    //console.log(html);
+			    var $ = cheerio.load(html);
+			    $('font').each(function(i, element){
+			    	var a = $(this).text()
+			    	var num = a.match(/\d+/)
+			    	console.log(num[0]);
+			    	scoreArr2.push(num[0])
+			    });
+			}
+			else{
+				console.log("empty")
+			}
+			request(webPrefix + webSufix3, function (err, rsp, html) {
+				if (!err && rsp.statusCode == 200) {
+				    //console.log(html);
+				    var $ = cheerio.load(html);
+				    $('font').each(function(i, element){
+				    	var a = $(this).text()
+				    	var num = a.match(/\d+/)
+				    	console.log(num[0]);
+				    	scoreArr3.push(num[0])
+				    });
+				}
+				else{
+					console.log("empty")
+				}
+			   	res.json( {
+			        s1: scoreArr1,
+			        s2: scoreArr2,
+			        s3: scoreArr3
+			    });
+			});
+		});
+	});
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
