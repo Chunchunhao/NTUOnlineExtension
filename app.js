22d28f50d4d5ae598849a40201b5ea1f305f6b94
oldwang <b00902126@ntu.edu.tw>
2014-08-15 15:27:47 +0800

    no class number fix

--- app.js
+++ app.js
@@ -80,6 +80,71 @@ app.get('/score/:year/:term/:ccode/:cnumber', function(req, res){
 	});
 });
 
+app.get('/score/:year/:term/:ccode', function(req, res){
+
+	var year = parseInt(req.params.year),
+		term = req.params.term,
+		CouCode = req.params.ccode
+
+	var scoreArr1 = [],
+		scoreArr2 = [],
+		scoreArr3 = []
+	var webPrefix = "http://if163.aca.ntu.edu.tw/eportfolio/student/Curve.asp?Year=" 
+	var webSufix1 = (year - 0 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class="
+	var webSufix2 = (year - 1 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class="
+	var webSufix3 = (year - 2 ) + "&Term=" + term + "&CouCode=" + CouCode + "&Class="
+
+	request(webPrefix + webSufix1, function (err, rsp, html) {
+		if (!err && rsp.statusCode == 200) {
+		    var $ = cheerio.load(html);
+		    $('font').each(function(i, element){
+		    	var a = $(this).text()
+		    	var num = a.match(/\d+/)
+		    	console.log(num[0]);
+		    	scoreArr1.push(num[0])
+		    });
+		} // Success fst year
+		else {
+			console.log("empty")
+		}
+		request(webPrefix + webSufix2, function (err, rsp, html) {
+			if (!err && rsp.statusCode == 200) {
+			    //console.log(html);
+			    var $ = cheerio.load(html);
+			    $('font').each(function(i, element){
+			    	var a = $(this).text()
+			    	var num = a.match(/\d+/)
+			    	console.log(num[0]);
+			    	scoreArr2.push(num[0])
+			    });
+			}
+			else{
+				console.log("empty")
+			}
+			request(webPrefix + webSufix3, function (err, rsp, html) {
+				if (!err && rsp.statusCode == 200) {
+				    //console.log(html);
+				    var $ = cheerio.load(html);
+				    $('font').each(function(i, element){
+				    	var a = $(this).text()
+				    	var num = a.match(/\d+/)
+				    	console.log(num[0]);
+				    	scoreArr3.push(num[0])
+				    });
+				}
+				else{
+					console.log("empty")
+				}
+			   	res.json( {
+			        s1: scoreArr1,
+			        s2: scoreArr2,
+			        s3: scoreArr3
+			    });
+			});
+		});
+	});
+});
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
